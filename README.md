584dcf83d1115abafb3412258bda5620197d8063
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-10-31 08:51:29 +0700

    finish retry

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 
 <![Loading....](./demoRedis/AnhMinhHoa/code_redis.png)>
 <![Loading....](./demoRedis/AnhMinhHoa/postman_Redis.png)>
+
+
+### retry
+
+<![Loading....](./spring-retry/AnhMinhHoa/code_retry.png)>
+
---
+++ spring-retry/src/main/java/com/example/springretry/demo/RemoteServiceNotAvailableException.java
@@ -0,0 +1,15 @@
+package com.example.springretry.demo;
+
+public class RemoteServiceNotAvailableException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RemoteServiceNotAvailableException(String msg) {
+		super(msg);
+	}
+
+	public RemoteServiceNotAvailableException(String msg, Exception ex) {
+		super(msg, ex);
+	}
+}
+
